4355a4959d3deb92c9fec033c09abb28e09a19c5
shubhamshri25 <105149595+shubhamshri25@users.noreply.github.com>
2023-03-03 13:55:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your code here
+const text = document.queryselector(#evaluatedText);
+const wordCount = document.queryselector(#wordCount);
+
+	function handelInput(e){
+		const sentence = e.target.value;
+		const match = sentence.mathc(/\S+/ig).length;
+		wordCount.textContent = match;
+	}
+
+text.addEventListner('input',handelInput);
