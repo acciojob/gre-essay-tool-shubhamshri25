b8193c0890bb8f6f76bbdaf1de5041c8edc262c9
shubhamshri25 <105149595+shubhamshri25@users.noreply.github.com>
2023-03-03 14:02:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 //your code here
-const text = document.queryselector("#evaluatedText");
-const h3 = document.queryselector("#wordCount");
+const text = document.querySelector("evaluatedText");
+const h3 = document.querySelector("#wordCount");
 
-	function handelInput(e){
+	function handeleInput(e){
 		const sentence = e.target.value;
 		const match = sentence.match(/\S+/ig).length;
 		h3.textContent = match;
 	}
 
-text.addEventListner("input",handelInput);
+text.addEventListener("input",handeleInput);
